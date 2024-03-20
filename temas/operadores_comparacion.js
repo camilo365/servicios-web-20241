4e9bddefdd6a3561d83fb56d61c8d2cce40cfa8b
camilo_C <juniorcamiloco0124@gmail.com>
2024-03-20 08:38:16 +0100

    metodos strings y comparacion

---
+++ temas/operadores_comparacion.js
@@ -0,0 +1,23 @@
+console.log("comparacion == ");
+let conunterone = 100;
+let countertwo = 200;
+let responseOne = conunterone == countertwo;
+console.log(responseOne);
+console.log("20" == 20);
+console.log("20" === 20);
+
+console.log("comparacion: no igual !=");
+let responseTwo = conunterone != "100"; //false
+console.log(responseTwo);
+let responseThree = conunterone !== "100"; //true
+console.log(responseThree);
+
+console.log("mayor que >");
+console.log(conunterone > 200); // counter = 100 ... false
+
+console.log("mayor igual que >= ");
+console.log(conunterone >= 100); //true
+
+console.log("menor que <");
+console.log(conunterone < 99);
+
---
+++ temas/strings.js
@@ -0,0 +1,25 @@
+
+let nombre = "camilo"
+const fullname = "juan camilo correa Tamayo";
+console.log(fullname.length)
+console.log(fullname[2])
+console.log(fullname.includes('Juan Camilo Correa Tamayo'))
+console.log(fullname.indexOf('camilo'))
+console.log(fullname.startsWith('Juan'))
+console.log(fullname.endsWith('Tamayo'))
+console.log(fullname.slice(2,7))
+console.log(fullname.slice(5))
+console.log(fullname.toUpperCase())
+console.log(fullname.toLowerCase())
+console.log(fullname.replace('camilo','hola'))
+console.log(fullname.repeat(4))
+console.log(fullname.trim())
+console.log(fullname.split())
+
+
+
+
+
+
+
+
