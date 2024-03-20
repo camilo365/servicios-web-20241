ec3d38bcb5613372b481e2c97a6f8405cb2ce8b5
camilo_C <juniorcamiloco0124@gmail.com>
2024-03-20 09:00:45 +0100

    comparacion

---
+++ temas/operadores_logicos.js
@@ -0,0 +1,16 @@
+console.log("operador AND &&")
+console.log("operador OR ||")
+console.log("operador NOT !")
+
+
+let counter = 10;
+
+if(counter > 100){
+    console.log(`el valor del counter es mayor a ${counter}`)
+}else{
+    console.log(`el valor del counter es menor a ${counter}`)
+}
+
+/* counter > 100 ? console.log(`el valor del counter es mayor a ${counter}`):console.log(`el valor del counter es menor a ${counter}`) */
+
+
