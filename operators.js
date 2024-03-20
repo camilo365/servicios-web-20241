cf46a7158d71868b98827e1448e3c4543f851b85
camilo_C <juniorcamiloco0124@gmail.com>
2024-03-20 07:05:06 +0100

    ultimo cambio

---
+++ operators.js
@@ -0,0 +1,53 @@
+let counter = 1000
+
+/* suma */
+
+counter = counter + 20;
+console.log(counter)
+counter += 30;
+console.log(counter)
+
+/* resta */
+
+counter = counter - 20;
+console.log(counter)
+counter -= 30;
+console.log(counter)
+
+/* multiplicacion */
+
+counter = counter * 3;
+console.log(counter)
+counter *= 4;
+console.log(counter) 
+
+/* division */
+
+counter = counter / 5;
+console.log(counter)
+counter /= 4;
+console.log(counter) 
+
+/* modulo */
+
+counter = counter % 3;
+console.log(counter)
+counter %= 3;
+console.log(counter) 
+
+/* exponenciacion */
+
+counter = counter ** 3;
+console.log(counter)
+counter **= 2;
+console.log(counter)
+
+
+
+let edad = "100"
+let age = 100
+if(edad == age){
+    console.log(true)
+}else{
+    console.log("false")
+}
